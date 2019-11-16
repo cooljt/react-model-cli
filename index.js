e0a485d58f438abb223b5321215e30ca72565698
Ti Jiang <jiang.ti1@husky.neu.edu>
2019-11-15 20:33:07 -0500

    add function to generation the model file

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
 const argv = require('minimist')(process.argv.slice(2));
+const ejs = require('ejs');
+
 const loadModelFile = require('./loadModel');
+const {tableTemplate, listTemplate, editorTemplate} = require('./template/modelTemplate');
+const {generateReactComponents} = require("./writeToFile");
 
 const obj = loadModelFile(argv);
+for (let i = 0; i < obj["classes"].length; i++) {
+    let tableComponent = ejs.render(tableTemplate, {entity: obj["classes"][i]["name"]});
+    let listComponent = ejs.render(listTemplate, {entity: obj["classes"][i]["name"], entityFields:obj["classes"][i]["fields"]});
+    let editorComponent = ejs.render(editorTemplate, {entity: obj["classes"][i]["name"]});
+    const components = {"Table": tableComponent, "List":listComponent, "Editor":editorComponent};
+    generateReactComponents(obj["classes"][i]["name"], components);
+}
+
+
 
 
 
---
+++ writeToFile.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const baseReactDir = process.cwd() + "/react/models/";
+const generateReactComponents = (name, components) => {
+    createDir(baseReactDir + name);
+    let filePath = baseReactDir + name + "/";
+    Object.keys(components).forEach((key) => {
+        let fileName = name + key + ".js";
+        writeFile(filePath + fileName, components[key]);
+    });
+}
+
+const createDir = (dirPath) => {
+    try {
+        fs.statSync(dirPath)
+    } catch (e) {
+        fs.mkdirSync(dirPath, {recursive: true});
+    }
+};
+
+const writeFile = (filePath, content) => {
+    fs.writeFile(filePath, content, (error) => {
+        if (error) {
+            console.log("Error : "+error);
+        } else {
+            console.log("Model file created for React component: " + filePath);
+        }
+    });
+}
+
+module.exports = {generateReactComponents};
