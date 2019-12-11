a5f83661af338b15565cb45d1fddc4698ce457b5
Ti Jiang <jiang.ti1@husky.neu.edu>
2019-12-11 16:32:38 -0500

    tweaks

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ for (let i = 0; i < obj["classes"].length; i++) {
     let listComponent = ejs.render(listTemplate, {entity: obj["classes"][i]["name"], entityFields:obj["classes"][i]["fields"]});
     let editorComponent = ejs.render(editorTemplate, {entity: obj["classes"][i]["name"]});
     let RESTService = ejs.render(RESTServiceTemplate, {entity: obj["classes"][i]["name"]});
+    
     const components = {"Table": tableComponent, "List":listComponent, "Editor":editorComponent};
     const services = {"Service": RESTService};
     generateReactComponents(obj["classes"][i]["name"], components);
