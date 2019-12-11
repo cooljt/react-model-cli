3912d73e3a2579d9aee2d44503fe22995cf03f7d
Ti Jiang <jiang.ti1@husky.neu.edu>
2019-12-11 12:34:41 -0500

    some change for model template

--- template/modelTemplate.js
+++ template/modelTemplate.js
@@ -1,32 +1,59 @@
 const tableTemplate = `
 import React from 'react';
 import <%=entity%>List from './<%=entity%>List.js';
-const <%=entity%>Table = () => 
-	<div>
-		<<%=entity%>List />
-	</div>
-export default <%=entity%>Table
+import <%=entity%>Service from '../../services/<%=entity%>/<%=entity%>Service.js';
+class <%=entity%>Table extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {data:[], isFetching:true}
+	}
+
+	componentDidMount() {
+		<%=entity%>Service.findAll<%=entity%>()
+		.then(data => this.setState({data:data, isFetching:false}));
+	}
+
+	render() {
+        return <div> 
+                {isFetching && <h3> Fetching data... </h3>}
+		        {!isFetching && <<%=entity%>List data={this.state.data}/>}
+               </div>
+	}
+}
+export default <%=entity%>Table;
 `;
 
 const listTemplate = `
 import React from 'react';
 import <%=entity%>Editor from './<%=entity%>Editor.js';
-const <%=entity%>List = (data) => 
-    <div>
+const <%=entity%>List = (data) => {
+
+    const renderData = (input) => {
+        return input.map((d) => {
+            return <ul style="list-style-type:none; display:flex;">
+            <% for (let i = 0; i < entityFields.length; i++) { %>
+            <l1>d.<%=entityFields[i]['name']%></l1>
+            <% } %>
+            <li>Button</li>
+        </ul>
+        });
+    }
+    
+    return <div>
         <div id="head">
-		    <ul style="list-style-type:none; display:flex;">
-			    <% for (let i = 0; i < entityFields.length; i++) { %>
-			    <l1><%=entityFields[i]['name']%></l1>
+            <ul style="list-style-type:none; display:flex;">
+                <% for (let i = 0; i < entityFields.length; i++) { %>
+                <l1><%=entityFields[i]['name']%></l1>
                 <% } %>
                 <l1>Operation</l1>
             </ul>
         </div>
         <div id="body">
-            <ul style="list-style-type:none; display:flex;">
-                #actual data
-            </ul>
+            {renderData(data)}
         </div>
-	</div>
+    </div>
+}
+    
 export default <%=entity%>List;
 `;
 const editorTemplate = `
